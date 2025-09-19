7a8e4c62aa665c9340c4e08db05368a6fb36d5df
pommicket <pommicket@gmail.com>
2025-09-19 13:14:48 -0400

    Fix pre-commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,6 +3,6 @@ was_modified() {
 	git diff --name-status HEAD -- $1 | grep -q 'M\s*'"$1"
 }
 
-if was_modified blankplays.js; then
-	npx eslint blankplays.js || exit 1
+if was_modified pub/blankplays.js; then
+	npx eslint || exit 1
 fi
